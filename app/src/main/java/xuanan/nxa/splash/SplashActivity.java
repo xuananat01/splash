31627c53c8c5f33c974ea0f79c10990bd0489493
xuananat01 <72687342+xuananat01@users.noreply.github.com>
2020-12-03 22:52:24 +0700

    Initial commit

---
+++ app/src/main/java/xuanan/nxa/splash/SplashActivity.java
@@ -0,0 +1,33 @@
+package xuanan.nxa.splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ImageView logo;
+    private static int splashTimeout=4000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        logo = (ImageView) findViewById(R.id.logo);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },splashTimeout);
+
+    }
+}
+
